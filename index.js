bfb11980acad06e5fcfaa2a1d8b13b6beb1bbd3d
sarah <sarah.akelo@student.moringaschool.com>
2025-03-12 20:43:37 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sarah',
+    post: 'Software Engineer',
+    age: 18
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // update and return employee object
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    //clone of the employee object
+    let clonedEmployee = { ...employee };
+    // Delete the specified key and return
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key and return
+    delete employee[key];
+    return employee;
+}
